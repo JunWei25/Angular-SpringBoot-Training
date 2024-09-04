dda54b692782b5b420e8e7c353acdede2aecb1dd
Tan Jun Wei <junwei2509@gmail.com>
2024-09-04 13:48:37 +0800

    classess and inheritance

---
+++ 04-classes/05-modules/Driver.js
@@ -0,0 +1,6 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const Customer_1 = require("./Customer");
+let newMyCustomer = new Customer_1.CustomerClass("Martin", "Dixon");
+console.log(newMyCustomer.firstName);
+console.log(newMyCustomer.lastName);
---
+++ 04-classes/05-modules/Driver.ts
@@ -0,0 +1,6 @@
+import { CustomerClass } from "./Customer";
+
+let newMyCustomer = new CustomerClass("Martin", "Dixon");
+
+console.log(newMyCustomer.firstName);
+console.log(newMyCustomer.lastName);
